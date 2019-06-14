420744cd9ae3664656996aee27eb10583db2ec56
Suyeol Jeon <devxoul@gmail.com>
2019-06-14 16:02:55 +0900

    Use Swift 5.0 in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "RxJSON",
+  platforms: [
+    .macOS(.v10_11), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+  ],
   products: [
     .library(name: "RxJSON", targets: ["RxJSON"]),
   ],
